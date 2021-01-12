0f16288c6770df16f80defb7b429c3bf94d459c5
Kenyon Bunker <46802577+Bunker-Kenyon@users.noreply.github.com>
2021-01-12 13:09:54 -0700

    php testing

---
+++ web/nav.php
@@ -0,0 +1,39 @@
+ <ul>
+    <li>
+      <a href="#">
+        <h2>Week 1</h2>
+        <p>Hello World!</p>
+      </a>
+    </li>
+    <li>
+      <a href="#">
+        <h2>Week 2</h2>
+        <p>TBD</p>
+      </a>
+    </li>
+    <li>
+      <a href="#">
+        <h2>Week 3</h2>
+        <p>TBD</p>
+      </a>
+    </li>
+    <li>
+      <a href="#">
+        <h2>Week 4</h2>
+        <p>TBD</p>
+      </a>
+    </li>
+    <li>
+      <a href="#">
+        <h2>Week 5</h2>
+        <p>TBD</p>
+      </a>
+    </li>
+    <li>
+      <a href="#">
+        <h2>Week 6</h2>
+        <p>TBD</p>
+      </a>
+    </li>
+    </li>
+  </ul>
