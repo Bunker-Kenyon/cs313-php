1cf7ee7f87ad073ecf5aaac4ac2b545afa6b4e37
Kenyon Bunker <46802577+Bunker-Kenyon@users.noreply.github.com>
2021-02-13 21:25:33 -0700

    Update myDB.sql

--- db/myDB.sql
+++ db/myDB.sql
@@ -7,7 +7,7 @@ CREATE DATABASE choreboard;
 --Create Chores Table
 CREATE TABLE public.chores (
     id SERIAL NOT NULL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
+    chore_name VARCHAR(100) NOT NULL,
     description TEXT NOT NULL,
     rewards_id INT NOT NULL REFERENCES public.rewards(id),
     date_created DATE NOT NULL,
@@ -44,6 +44,25 @@ CREATE TABLE public.user
 CREATE TABLE public.household (
 	id SERIAL NOT NULL PRIMARY KEY,
 	name_of_household VARCHAR(250) NOT NULL UNIQUE
+);
+
+--Create table chore_library
+CREATE TABLE public.chore_library (
+    id SERIAL NOT NULL PRIMARY KEY,
+    chore_name VARCHAR(100) NOT NULL UNIQUE,
+    description TEXT NOT NULL,
+    is_repeatable BOOLEAN NOT NULL,
+    xp_reward INT NOT NULL,
+    reward_library_id INT NOT NULL REFERENCES public.reward_library(id),
+    household_id INT NOT NULL REFERENCES piblic.household(id)
+);
+
+--Create table reward_library
+CREATE TABLE public.reward_library (
+    id SERIAL NOT NULL PRIMARY KEY,
+    reward_name VARCHAR (100) NOT NULL UNIQUE,
+    description TEXT NOT NULL,
+    household_id INT NOT NULL REFERENCES piblic.household(id)
 );
 
 --adding new column xp_user to user
@@ -109,3 +128,52 @@ DROP COLUMN chores_id;
 
 ALTER TABLE public.user
 DROP COLUMN rewards_id;
+
+--Select chores from chore library
+SELECT chore_library.chore_name, chore_library.description, chore_library.xp_reward, 
+	reward_library.id, reward_library.reward_name, household.id, household.name_of_household
+    FROM chore_library
+    LEFT JOIN reward_library
+    ON chore_library.reward_library_id=reward_library.id
+	LEFT JOIN public.household
+	ON chore_library.household_id=household.id
+	Where household.name_of_household = 'Bunker'
+    ORDER BY chore_library.chore_name
+
+--Select rewards from reward library
+SELECT reward_library.id, reward_library.reward_name, reward_library.description, household.id, 
+	household.name_of_household
+    FROM reward_library
+	LEFT JOIN public.household
+	ON reward_library.household_id=household.id
+	Where household.name_of_household = 'Bunker'
+    ORDER BY reward_library.reward_name
+
+--Insert into Rewards Library
+INSERT INTO reward_library(reward_name, description, household_id)
+    VALUES ('Picture Walk', 'Go on a walk and take pictures', 2);
+
+--Insert into chore library
+INSERT INTO chore_library(chore_name, description, is_repeatable, xp_reward, reward_library_id, household_id)
+    Values ('Mow the Grass', 'Mow the grass in the back yard', false, 100, 6, 2);
+
+--Update Rewards Library
+UPDATE reward_library
+    SET reward_name = 'Family Walk', description = 'Go on walk with family'
+    WHERE id = 1 AND household_id = 1;
+
+--Update chores library
+UPDATE chore_library
+	SET chore_name = 'Bedroom', description ='Clean room completely, except for vacuuming or mopping.', is_repeatable = TRUE,
+        xp_reward = 25, reward_library_id = 1
+    WHERE id = 27 AND household_id = 1;
+
+--Delete from chore library
+DELETE FROM chore_library
+    WHERE id = 37;
+    
+--Delete from rewards library
+    --NOTE: can only delete this if no cores with this reward id exist
+DELETE FROM reward_library
+    WHERE id = 4;
+
